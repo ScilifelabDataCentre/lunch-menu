a2a1acbe7629d5af81738b3fcf77994f83278311
Linus Östberg <linus@talavis.eu>
2014-10-07 10:02:10 +0200

    Fixed Karolina. Same problem as in the other Sodexo restaurants

--- parse_menus2.py
+++ parse_menus2.py
@@ -259,9 +259,10 @@ def parse_karolina(filename, weekday, tomorrow, day, month) :
 
     start = False
     today = '{wday} {iday} {mon}'.format(wday = weekday, iday = day, mon = month)
+    today_alt = '{wday}  {iday} {mon}'.format(wday = weekday, iday = day, mon = month)
     current = list()
     for line in open(filename, encoding='utf8') :
-        if today in line.lower() :
+        if today in line.lower() or today_alt in line.lower() :
             note('Karolina - day found')
             start = True
             continue
