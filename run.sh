16d372adac88ef9da0ac1022929714b8e4880b98
Linus Östberg <linus@talavis.eu>
2015-02-03 12:32:52 +0100

    Added a zero before single-digit weeks for matmakarna.

--- run.sh
+++ run.sh
@@ -30,18 +30,28 @@
 
 PARSER=./parse_menus2.py
 OUT_FILE=index.html
-
 # an attempt to compensate for the instability in the naming of the menu page for glada restaurangen
 function get_glada {
     GLADA=glada.html
     week=$(($(date +%W | sed 's/^0*//')+1))
     year=`date +%Y`
-    wget -nv "http://www.dengladarestaurangen.se/vecka-${week}" -O ${GLADA} 1&> /dev/null
+    wget -nv "http://www.dengladarestaurangen.se/meny-vecka-${week}" -O ${GLADA} 1&> /dev/null
     if [ -s ${GLADA} ] ; then
 	return
     fi
-    wget -nv "http://www.dengladarestaurangen.se/meny-vecka-${week}" -O ${GLADA} 1&> /dev/null
+    wget -nv "http://www.dengladarestaurangen.se/vecka-${week}" -O ${GLADA} 1&> /dev/null
 }
+
+function get_matmakarna {
+    year2=`date +%y`
+    week=$(($(date +%W | sed 's/^0*//')+1))
+    if [ ${#week} -eq 1 ] ; then
+	week=0${week}
+    fi
+    echo $week
+    wget -nv "http://www.matmakarna.nu/Veckomeny_Restaurang_Matmakarna/index_Vecka_${week}${year2}.html" -O matmakarna.html 1&> /dev/null
+}
+    
 
 function dl_menus {
     wget -nv "http://mollanasiankok.se/veckansmeny.html" -O mollan.html 1&> /dev/null
@@ -49,8 +59,7 @@ function dl_menus {
     wget -nv "http://gastrogate.com/restaurang/restauranghjulet/page/3/" -O hjulet.html 1&> /dev/null
     wget -nv "http://restaurangkonigs.se/" -O konigs.html 1&> /dev/null
     get_glada
-    year2=`date +%y`
-    wget -nv "http://www.matmakarna.nu/Veckomeny_Restaurang_Matmakarna/index_Vecka_${week}${year2}.html" -O matmakarna.html 1&> /dev/null
+    get_matmakarna
     wget -nv "http://mmcatering.nu/mfs-kafe-kok/meny" -O mf.html 1&> /dev/null
     wget -nv "http://gastrogate.com/restaurang/tango/page/3/" -O tango.html 1&> /dev/null
     wget -nv "http://restaurang-ns.com/restaurang-nanna-svartz/" -O nanna.html 1&> /dev/null
@@ -63,4 +72,4 @@ function dl_menus {
 
 dl_menus
 
-${PARSER} glada=glada.html karolina=karolina.html hjulet=hjulet.html jons=jons.html konigs=konigs.html mollan=mollan.html nanna=nanna.html subway=true mf=mf.html jorpes=true alfred=alfred.html tango=tango.html 61an=61an.html haga=true, stories=true > ${OUT_FILE}
+${PARSER} glada=glada.html karolina=karolina.html hjulet=hjulet.html jons=jons.html konigs=konigs.html mollan=mollan.html nanna=nanna.html subway=true mf=mf.html jorpes=true alfred=alfred.html tango=tango.html 61an=61an.html haga=true, stories=true matmakarna=matmakarna.html > ${OUT_FILE}
