05d149622dc047e4bec99ffaa1129396e9589c11
Linus Östberg <linus@talavis.eu>
2017-10-31 09:54:10 +0100

    Style changes and more robust parser management
    Most pylint errors fixed.
    Some refactoring to move code to functions, preparing for future testing.
    Exception handling whenever calling a parser function to prevent crashing in the middle of menu generation.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2014-2016, Linus Östberg
+# Copyright (c) 2014-2017, Linus Östberg
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -27,27 +27,51 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+'''
+Main script for choosing what restaurant parsers to use
+'''
 
-import codecs
-import string
 import sys
 
 import parser as ps
 
-# page end, ie closing tags etc
-def page_end() :
+
+def activate_parsers(restaurants, restaurant_data, mapper):
+    '''
+    Run the wanted parsers
+    '''
+    # print restaurants
+    for i in range(len(mapper)):
+        if mapper[i][0] in restaurants:
+            try:
+                to_use = restaurant_data[[x[0] for x in restaurant_data].index([x[0] for x in mapper][i])]
+                print('\n'.join(mapper[i][1](to_use)))
+            except Exception as err:
+                sys.stderr.write('E in {}: {}\n'.format(mapper[i][0], err))
+
+
+def page_end():
+    '''
+    Print the closure of tags etc
+    '''
     lines = list()
-    lines.append('<div class="endnote">Code available at <a href="https://github.com/talavis/kimenu">Github</a>. Patches are very welcome.</div>')
+    lines.append('<div class="endnote">Code available at ' +
+                 '<a href="https://github.com/talavis/kimenu">' +
+                 'Github</a>. Patches are very welcome.</div>')
     lines.append('</body>')
     lines.append('</html>')
     return lines
 
-# page start, including google translate support
-def page_start(weekday, day, month) :
+
+def page_start(weekday, day, month):
+    '''
+    Print the initialisation of the page
+    '''
     lines = list()
     lines.append('<html>')
     lines.append('<head>')
-    lines.append('<title>Dagens mat p&aring; KI - {date}</title>'.format(date = ps.fix_for_html(weekday.capitalize() + ' ' + str(day) + ' ' + str(month))))
+    date = ps.fix_for_html(weekday.capitalize() + ' ' + str(day) + ' ' + str(month))
+    lines.append('<title>Dagens mat p&aring; KI - {}</title>'.format(date))
     lines.append('<link href="styles.css" rel="stylesheet" type="text/css">')
     lines.append('<style type="text/css"></style>')
     lines.append('</head>')
@@ -56,27 +80,45 @@ def page_start(weekday, day, month) :
     lines.append('')
     return lines
 
-def print_usage(supported) :
-    '''Print description of syntax'''
+
+def parse_restaurant_names(rest_names, mapper):
+    '''
+    Decide what restaurants to generate menus for
+    '''
+    restaurants = list()
+    for param in rest_names:
+        if param not in (x[0] for x in mapper):
+            raise ValueError('{} not a valid restaurant'.format(param))
+        restaurants.append(param.lower())
+    return restaurants
+
+
+def print_usage(supported):
+    '''
+    Print description of syntax
+    '''
     sys.stderr.write('Usage: {} restaurant1 [restaurant2] \n'.format(sys.argv[0]))
     sys.stderr.write('Supported restaurants: {}\n'.format(', '.join(sorted(supported))))
     sys.stderr.write('write all to generate all supported restaurants\n')
     sys.stderr.write('-r filepath to use another restaurant list\n')
 
-def read_restaurants(filename) :
-    '''Read the list of restaurants'''
-    '''Read a tsv file with the columns:
-    [0] identifier [1] Name [2] URL [3] Menu URL [4] OSM URL'''
 
+def read_restaurants(filename):
+    '''
+    Read the list of restaurants
+    Read a tsv file with the columns:
+    [0] identifier [1] Name [2] URL [3] Menu URL [4] OSM URL
+    '''
     restaurants = list()
-    with open(filename) as infile :
-        for line in infile :
-            if line.lstrip()[0] == '#' :
+    with open(filename) as infile:
+        for line in infile:
+            if line.lstrip()[0] == '#':
                 continue
             restaurants.append(line.rstrip().split('\t'))
     return restaurants
-    
-if __name__ == '__main__' :
+
+
+if __name__ == '__main__':
     MAPPER = (('jorpes', ps.parse_jorpes), ('glada', ps.parse_glada),
               ('haga', ps.parse_haga), ('hjulet', ps.parse_hjulet),
               ('jons', ps.parse_jons), ('karolina', ps.parse_karolina),
@@ -85,39 +127,30 @@ if __name__ == '__main__' :
               ('subway', ps.parse_subway), ('61an', ps.parse_61an),
               ('alfred', ps.parse_alfred), ('stories', ps.parse_stories),
               ('matmakarna', ps.parse_matmakarna), ('tango', ps.parse_tango))
-    
-    if len(sys.argv) < 2 or '-h' in sys.argv :
+
+    if len(sys.argv) < 2 or '-h' in sys.argv:
         print_usage((x[0] for x in MAPPER))
         sys.exit()
 
-    if '-r' in sys.argv :
-        RESTAURANTS = sys.argv[sys.argv.index('-r') + 1]
-    else :
-        RESTAURANTS = 'restaurants.txt'
-        
-    # get filenames
-    restaurant_data = read_restaurants(RESTAURANTS)
-    restaurants = list()
-    if 'all' in sys.argv[1:] :
-        inrest = (x[0] for x in MAPPER)
-    elif '-r' in sys.argv :
-        inrest = sys.argv[1:sys.argv.index('-r')] + sys.argv[sys.argv.index('-r')+2:]
-    else :
-        inrest = sys.argv[1:]
-    
-    for param in inrest :
-        if param not in (x[0] for x in MAPPER) :
-            if param != '-r' :
-                print_usage((x[0] for x in MAPPER))
-                sys.exit()
-        if param != '-r' :
-            restaurants.append(param.lower())
+    if '-r' in sys.argv:
+        RESTAURANTS_FILE = sys.argv[sys.argv.index('-r') + 1]
+    else:
+        RESTAURANTS_FILE = 'restaurants.txt'
 
+    RESTAURANT_DATA = read_restaurants(RESTAURANTS_FILE)
+    if 'all' in sys.argv[1:]:
+        REST_NAMES_IN = (x[0] for x in MAPPER)
+    else:
+        REST_NAMES_IN = [param for param in sys.argv[1:] if param != '-r']
+
+    try:
+        REST_NAMES = parse_restaurant_names(REST_NAMES_IN, MAPPER)
+    except ValueError as err:
+        sys.stderr.write('E: {}'.format(err))
+        print_usage((x[0] for x in MAPPER))
+        sys.exit(1)
+
+    # print the menus
     print('\n'.join(page_start(ps.get_weekday(), str(ps.get_day()), ps.get_month())))
-    # print restaurants
-    for i in range(len(MAPPER)) :
-        if MAPPER[i][0] in restaurants :
-            print('\n'.join(MAPPER[i][1](restaurant_data[[x[0] for x in restaurant_data].index([x[0] for x in MAPPER][i])])))
-
+    activate_parsers(REST_NAMES, RESTAURANT_DATA, MAPPER)
     print('\n'.join(page_end()))
-    
