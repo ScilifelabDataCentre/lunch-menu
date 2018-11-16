10d7cb55f7cea84e2e7e1a17b2af8f2fdc7f4d65
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2018-11-16 14:52:52 +0100

    Fix Karolina Exception handling

--- parser.py
+++ parser.py
@@ -314,7 +314,6 @@ def parse_karolina(resdata):
         page_req = requests.get(resdata[3])
         if page_req.status_code != 200:
             raise IOError('Bad HTTP responce code')
-
         soup = BeautifulSoup(page_req.text, 'html.parser')
         days = soup.find('table', {'class':'table lunch_menu animation'})
         day = days.find('tbody', {'class':'lunch-day-content'})
@@ -323,7 +322,7 @@ def parse_karolina(resdata):
             lines.append(dish.get_text().strip().split(':')[1] + '<br/>')
 
     except Exception as err:
-        sys.stderr.write(err)
+        sys.stderr.write(str(err) + '\n')
 
     lines += restaurant_end()
     return lines
