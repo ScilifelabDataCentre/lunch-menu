fb76317bdaed560e653ac46791dbb2b95c908c33
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2019-08-23 09:45:42 +0200

    flask update

--- flask_app.py
+++ flask_app.py
@@ -3,17 +3,26 @@ from flask_caching import Cache
 
 import main
 
+from flask import Flask
+from flask_caching import Cache
+
+import main
+
 app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 @app.route('/')
 def display_available():
-    return main.page_start("Restaurant Menu Parser")[0] + """
-    <p><a href="ki">KI (Solna)</a></p>
-    <p><a href="uu">UU (BMC)</a></p>
-  </body>
-</html>
-    """
+    content = ('<html>' +
+               '<head>' +
+               '<title>Restaurant Menu Parser</title>' +
+               '</head>' +
+               '<body>' +
+               '<p><a href="ki">KI (Solna)</a></p>' + 
+               '<p><a href="uu">UU (BMC)</a></p>' +
+               '</body>' +
+               '</html>')
+    return content
 
 @app.route('/ki')
 @cache.cached(timeout=3600)
