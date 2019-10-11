7c6a72e3b5c4315dceb5fffffcf56a05e0d707a8
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2019-10-11 07:34:08 +0200

    Sort restaurant entries.

--- frontend/menupage/src/App.vue
+++ frontend/menupage/src/App.vue
@@ -1,7 +1,10 @@
 <template>
 <div id="app">
-    <restaurant-entry v-for="restaurant in restaurants" :key="restaurant.name" :restaurant_info="restaurant">
-    </restaurant-entry>
+  <div v-if="!restaurants">Waiting for data from API...</div>
+  <restaurant-entry v-for="restaurant in sortedSolna" :key="restaurant.name" :restaurant_info="restaurant">
+  </restaurant-entry>
+  <restaurant-entry v-for="restaurant in sortedUppsala" :key="restaurant.name" :restaurant_info="restaurant">
+  </restaurant-entry>
   <div class="endnote">Code available at <a href="https://github.com/talavis/lunch-menu">Github</a>.
     Patches are very welcome.
   </div>
@@ -23,6 +26,17 @@ export default {
       active: ['bikupan', 'hjulet']
     }
   },
+  computed: {
+    sortedSolna () {
+      let chosen = this.restaurants.filter((rest) => rest.campus == 'Solna')
+      return chosen.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
+    },
+    sortedUppsala () {
+      let chosen = this.restaurants.filter((rest) => rest.campus == 'Uppsala')
+      return chosen.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
+    }
+  },
+
   mounted () {
     axios
       .get('http://scilifelab-lunches.herokuapp.com/api/restaurants')
