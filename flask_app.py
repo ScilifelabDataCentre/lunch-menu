081420ad51a3e3b04d3ac13b624c89bd58af5a5f
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-03-22 17:11:00 +0100

    Updated API

--- flask_app.py
+++ flask_app.py
@@ -4,47 +4,28 @@ from flask_cors import CORS
 
 import main
 
-
 app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
+
 @app.route('/')
-def display_available():
-    content = ('<html>' +
-               '<head>' +
-               '<title>Restaurant Menu Parser</title>' +
-               '</head>' +
-               '<body>' +
-               '<p><a href="ki">Campus Solna (KI)</a></p>' +
-               '<p><a href="uu">Campus Uppsala (BMC)</a></p>' +
-               '</body>' +
-               '</html>')
-    return content
-
-
-@app.route('/restaurants')
 @cache.cached(timeout=3600)
-def api_list_restaurants():
-    return jsonify(main.list_restaurants())
+def nbis_list_entities():
+    return jsonify({'entities': ['restaurant']})
 
 
-@app.route('/restaurant/<name>')
+@app.route('/restaurant/')
 @cache.cached(timeout=3600)
-def api_get_restaurant(name):
+def nbis_api_list_restaurants():
+    return jsonify({'restaurants': main.list_restaurants()})
+
+
+@app.route('/restaurant/<name>/')
+@cache.cached(timeout=3600)
+def nbis_api_get_restaurant(name):
     data = main.get_restaurant(name)
     if not data:
-        abort(404)
-    return jsonify(data)
-
-
-@app.route('/ki')
-@cache.cached(timeout=3600)
-def make_menu_ki():
-    return main.gen_ki_menu()
-
-
-@app.route('/uu')
-@cache.cached(timeout=3600)
-def make_menu_uu():
-    return main.gen_uu_menu()
+        abort(status=404)
+    data['menu'] = [{'dish': entry} for entry in data['menu']]
+    return jsonify({'restaurant': data})
