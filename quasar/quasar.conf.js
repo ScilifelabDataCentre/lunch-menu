16202fa9a696547c9995489fa9b5b602276e9746
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-07-20 16:15:57 +0200

    Remove a few unused rows

--- quasar/quasar.conf.js
+++ quasar/quasar.conf.js
@@ -30,14 +30,6 @@ module.exports = function (/* ctx */) {
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
-      // 'ionicons-v4',
-      // 'mdi-v5',
-      // 'fontawesome-v5',
-      // 'eva-icons',
-      // 'themify',
-      // 'line-awesome',
-      // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
-
       'roboto-font', // optional, you are not bound to it
       'material-icons', // optional, you are not bound to it
     ],
