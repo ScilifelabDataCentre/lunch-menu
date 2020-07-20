2492569e63ec10ec171c408b4b9053e282fc795f
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-07-20 16:12:37 +0200

    Change link to data centre at top to dropdown

--- quasar/src/layouts/MainLayout.vue
+++ quasar/src/layouts/MainLayout.vue
@@ -6,74 +6,84 @@
         {{ today }}
       </span>
       <q-space />
-          <q-btn-dropdown dense
-                          flat
-                          no-wrap
-                          no-caps
-			  text-color="secondary"
-                          icon="location_on"
-                          class="q-ml-sm pull-right">
-            <q-list>
-              <q-item>
-                <q-item-section avatar>
-                  <q-btn round
-                         :icon="showSolna ? 'location_on' : 'location_off'"
-                         :color="showSolna ? 'positive' : 'grey-2'"
-                         :text-color="showSolna ? 'white' : 'black'"
-                         @click="showSolna = !showSolna" />
-                </q-item-section>
-                <q-item-section>
-                  <q-item-label>Solna</q-item-label>
-                  <q-item-label caption>Show restaurants at KI Solna</q-item-label>
-                </q-item-section>
-              </q-item>
+      <q-btn-dropdown dense
+                      flat
+                      no-wrap
+                      no-caps
+		      text-color="secondary"
+                      icon="location_on"
+                      class="q-ml-sm pull-right">
+        <q-list>
+          <q-item>
+            <q-item-section avatar>
+              <q-btn round
+                     :icon="showSolna ? 'location_on' : 'location_off'"
+                     :color="showSolna ? 'positive' : 'grey-2'"
+                     :text-color="showSolna ? 'white' : 'black'"
+                     @click="showSolna = !showSolna" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Solna</q-item-label>
+              <q-item-label caption>Show restaurants at KI Solna</q-item-label>
+            </q-item-section>
+          </q-item>
 
-              <q-item>
-                <q-item-section avatar>
-                  <q-btn round
-                         :icon="showUppsala ? 'location_on' : 'location_off'"
-                         :color="showUppsala ? 'positive' : 'grey-2'"
-                         :text-color="showUppsala ? 'white' : 'black'"
-                         @click="showUppsala = !showUppsala" />
-                </q-item-section>
-                <q-item-section>
-                  <q-item-label>Uppsala</q-item-label>
-                  <q-item-label caption>Show restaurants at BMC</q-item-label>
-                </q-item-section>
-              </q-item>
+          <q-item>
+            <q-item-section avatar>
+              <q-btn round
+                     :icon="showUppsala ? 'location_on' : 'location_off'"
+                     :color="showUppsala ? 'positive' : 'grey-2'"
+                     :text-color="showUppsala ? 'white' : 'black'"
+                     @click="showUppsala = !showUppsala" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Uppsala</q-item-label>
+              <q-item-label caption>Show restaurants at BMC</q-item-label>
+            </q-item-section>
+          </q-item>
 
-              <q-separator inset spaced />
+          <q-separator inset spaced />
 
-              <q-item>
-                <q-item-section avatar>
-                  <q-btn round
-                         :icon="onlyFavourites ? 'favorite' : 'favorite_border'"
-                         :color="onlyFavourites ? 'positive' : 'grey-2'"
-                         :text-color="onlyFavourites ? 'white' : 'black'"
-                         @click="onlyFavourites = !onlyFavourites" />
-                </q-item-section>
-                <q-item-section>
-                  <q-item-label>Favourites Only</q-item-label>
-                  <q-item-label caption>Show only favourited restaurants</q-item-label>
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </q-btn-dropdown>
+          <q-item>
+            <q-item-section avatar>
+              <q-btn round
+                     :icon="onlyFavourites ? 'favorite' : 'favorite_border'"
+                     :color="onlyFavourites ? 'positive' : 'grey-2'"
+                     :text-color="onlyFavourites ? 'white' : 'black'"
+                     @click="onlyFavourites = !onlyFavourites" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Favourites Only</q-item-label>
+              <q-item-label caption>Show only favourited restaurants</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
 
-          <q-separator vertical />
-          <q-btn
-            dense
-            flat
-            no-wrap
-            no-caps
-            color="primary"
-            label="SciLifeLab Data Centre"
-            class="q-ml-sm pull-right"
-            type="a"
-            target="_blank"
-            href="https://www.scilifelab.se/data/"
-            />
-        </q-toolbar>
+      <q-separator vertical />
+      <q-btn-dropdown flat
+                      label="About"
+                      class="text-primary">
+        <q-list>
+          <q-item tag="a"
+                  href="https://www.scilifelab.se/data/"
+                  target="_blank">
+            <q-item-section>
+              <q-item-label>SciLifeLab Data Centre</q-item-label>
+              <q-item-label caption>Developers of the menu page</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item tag="a"
+                  href="https://github.com/ScilifelabDataCentre/lunch-menu"
+                  target="_blank">
+            <q-item-section>
+              <q-item-label>Github</q-item-label>
+              <q-item-label caption>Code, issues, requests</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+    </q-toolbar>
   </q-header>
   <q-page-container>
     <router-view />
