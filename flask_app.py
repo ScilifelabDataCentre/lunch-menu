d2b9b8becdd15398d71d3374376559021325da81
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-08-26 15:44:09 +0200

    First implementation of a slack bot response

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
 from flask import Flask, abort, jsonify
+import flask
 from flask_caching import Cache
 from flask_cors import CORS
 
@@ -29,3 +30,26 @@ def get_restaurant(name):
         abort(status=404)
     data['menu'] = [{'dish': entry} for entry in data['menu']]
     return jsonify({'restaurant': data})
+
+@app.route('/api/slack/', methods=['POST'])
+def handle_slack_request():
+    import logging
+    logging.error(f'incoming form: {flask.request.form}')
+    command_text = flask.request.form['text']
+    available = [entry['identifier'] for entry in main.list_restaurants()]
+
+    if command_text not in available:
+        text = f'Usage: `/lunch-menu <identifier>`. Available restaurants:\n\n'
+        for entry in main.list_restaurants():
+            text += f'- {entry["name"]}: `{entry["identifier"]}`\n'
+    else:
+        restaurant_data = main.get_restaurant(command_text)
+        text = f'{restaurant_data["title"]}:\n'
+        for dish in restaurant_data['menu']:
+            text += f'- {dish}\n'
+
+    response = {"blocks": [{"type": "section",
+			    "text": {
+				"type": "mrkdwn",
+				"text": text}}]}
+    return flask.jsonify(response)
