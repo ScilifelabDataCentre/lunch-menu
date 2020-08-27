06217e39ccb216e98d2cd51d0207709f2fc8c90d
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-08-27 08:57:58 +0200

    Correct module name

--- flask_app.py
+++ flask_app.py
@@ -3,12 +3,13 @@ from flask_caching import Cache
 from flask_cors import CORS
 
 import main
+import slack
 
 app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-app.register_blueprint(dataset.blueprint, url_prefix='/api/slack')
+app.register_blueprint(slack.blueprint, url_prefix='/api/slack')
 
 @app.route('/api/')
 @cache.cached(timeout=10800)
