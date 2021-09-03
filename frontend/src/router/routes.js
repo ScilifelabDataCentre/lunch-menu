027283b354c8b0b3098b1830961fdd4d07a5312a
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-09-03 23:02:13 +0200

    Add solna/uppsala routes

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -8,6 +8,30 @@ const routes = [
     ]
   },
 
+  {
+    path: '/solna',
+    alias: '/ki',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      {
+        path: '',
+        component: () => import('pages/Index.vue'),
+      }
+    ],
+  },
+
+  {
+    path: '/uppsala',
+    alias: '/uu',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      {
+        path: '',
+        component: () => import('pages/Index.vue'),
+      }
+    ],
+  },
+
   // Always leave this as last one,
   // but you can also remove it
   {
