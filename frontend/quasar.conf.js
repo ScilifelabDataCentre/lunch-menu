8e90722c0196b0661a0e7b516d4d78822c108c00
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-09-03 22:48:27 +0200

    Add missing ,

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -91,7 +91,7 @@ module.exports = configure(function (ctx) {
     framework: {
       config: {
         dark: 'auto' // or Boolean true/false
-      }
+      },
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
 
