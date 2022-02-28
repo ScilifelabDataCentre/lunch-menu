f3d5c80ee514a8f113de7d31f33f3c10fbabe5ef
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2022-02-28 15:22:19 +0100

    Add support for ip from X-Forwarded-For

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -1,6 +1,9 @@
+import os
+
 import flask
 import flask_caching
 import flask_cors
+import werkzeug.middleware.proxy_fix
 
 import main
 import slack
@@ -9,21 +12,29 @@ app = flask.Flask(__name__)
 cache = flask_caching.Cache(app, config={"CACHE_TYPE": "simple"})
 cors = flask_cors.CORS(app, resources={r"/*": {"origins": "*"}})
 
+if os.env.get("REVERSE_PROXY", False):
+    app.wsgi_app = proxy_fix.ProxyFix(app.wsgi_app)
+
 app.register_blueprint(slack.blueprint, url_prefix="/api/slack")
 
 
 @app.route("/api/")
 @cache.cached(timeout=10800)
 def list_entities():
-    return flask.jsonify({"entities": ["restaurant"],
-                    "url": flask.url_for("list_entities", _external=True)})
+    return flask.jsonify(
+        {"entities": ["restaurant"], "url": flask.url_for("list_entities", _external=True)}
+    )
 
 
 @app.route("/api/restaurant")
 @cache.cached(timeout=10800)
 def list_restaurants():
-    return flask.jsonify({"restaurants": main.list_restaurants(),
-                    "url": flask.url_for("list_restaurants", _external=True)})
+    return flask.jsonify(
+        {
+            "restaurants": main.list_restaurants(),
+            "url": flask.url_for("list_restaurants", _external=True),
+        }
+    )
 
 
 @app.route("/api/restaurant/<name>")
@@ -33,5 +44,6 @@ def get_restaurant(name):
     if not data:
         flask.abort(status=404)
     data["menu"] = [{"dish": entry} for entry in data["menu"]]
-    return flask.jsonify({"restaurant": data,
-                    "url": flask.url_for("get_restaurant", name=name, _external=True)})
+    return flask.jsonify(
+        {"restaurant": data, "url": flask.url_for("get_restaurant", name=name, _external=True)}
+    )
